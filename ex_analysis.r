4b1a095491af080b4cf12436d0327d695106c8a3
Kenny Flagg <kenneth.flagg@msu.montana.edu>
2019-12-20 23:49:13 -0700

    Week 5 check-in.

--- ex_analysis.r
+++ ex_analysis.r
@@ -7,25 +7,22 @@ library(INLA) # For model fitting.
 #library(maptools) # Provides as.SpatialPolygons method for owin.
 
 # Plot the observed point pattern and its window.
-# TODO: add an informative plot title.
 pdf('figures/bei.pdf', width = 12, height = 6)
 par(mar = c(0, 0, 2, 0))
-plot(bei)
+plot(bei, main = expression(paste(italic('Beilschmiedia pendula Lauraceae'), ' Locations')))
 dev.off()
 
 # Plot the elevation surface.
-# TODO: add an informative plot title.
 pdf('figures/beielev.pdf', width = 12, height = 6)
 par(mar = c(0, 0, 2, 0))
-plot(bei.extra$elev)
+plot(bei.extra$elev, main = 'Elevation')
 points(bei, pch = '.', col = 'white')
 dev.off()
 
 # Plot the gradient surface.
-# TODO: add an informative plot title.
 pdf('figures/beigrad.pdf', width = 12, height = 6)
 par(mar = c(0, 0, 2, 0))
-plot(bei.extra$grad)
+plot(bei.extra$grad, main = 'Gradient')
 points(bei, pch = '.', col = 'white')
 dev.off()
 
@@ -68,7 +65,7 @@ dev.off()
 # The covariates are measured on a lattice which does not match up to the mesh
 # we'll use for the intensity function. There isn't a well-developed way to
 # handle the change-of-support within the model fit in INLA (e.g. by adding a
-# level to the model hierarchy to predict the covariates a the nodes).
+# level to the model hierarchy to predict the covariates at the nodes).
 # However, these covariate surfaces are smooth enough that little will be lost
 # by using piecewise linear interpolation to find values at the nodes.
 # (Using an overly coarse mesh would be a bigger problem than interpolation).
@@ -103,7 +100,7 @@ plot(im(t(inla.mesh.project(bei_proj, bei_mesh_elev)),
         yrange = Frame(bei)$y,
         unitname = c('meter', 'meters')),
         main = 'Piecewise Linear Approximation of Elevation')
-plot(Window(bei), border = 'white', add = TRUE)
+plot(Window(bei), border = '#80808080', add = TRUE)
 points(bei, pch = '.', col = 'white')
 dev.off()
 
@@ -116,9 +113,8 @@ plot(im(t(inla.mesh.project(bei_proj, bei_mesh_grad)),
         unitname = c('meter', 'meters')),
         main = 'Piecewise Linear Approximation of Gradient')
 plot(Window(bei), border = 'white', add = TRUE)
-points(bei, pch = '.', col = 'white')
+points(bei, pch = '.', col = '#80808080')
 dev.off()
-
 
 
 ######################################
@@ -132,6 +128,20 @@ bei_pts <- cbind(bei$x, bei$y)
 # The sum of these will be the number of pseudodata points.
 bei_mesh_size <- bei_mesh$n
 bei_n_events <- nrow(bei_pts)
+
+# Create the psuedodata. This is a vector giving the count of events at each
+# pseudopoint location, that is 0 at each mesh node and 1 at each event.
+bei_pseudodata <- c(rep(0, bei_mesh_size), rep(1, bei_n_events))
+
+# Get the numerical integration weights for the SPDE approach.
+# Because the whole region was fully observed (constant sampling effort),
+# these are left as is. If parts of the region were unobserved, these would be
+# multiplied by the observed proportion of the area represented by the node.
+bei_int_weights <- diag(inla.mesh.fem(bei_mesh)$c0)
+
+# Concatenate the weight vector with a vector of zeros for the observed events.
+# This is the vector of Poisson exposure parameters for the pseudodata.
+bei_pseudodata_exp <- c(bei_int_weights, rep(0, bei_n_events))
 
 # Compute the barycentric coordinates of the observed events
 # (i.e. project into the space spanned by the basis of mesh nodes).
@@ -148,20 +158,6 @@ bei_int_matrix <- sparseMatrix(
 # Bind the node and event coordinates into a single matrix of pseudodata
 # locations in barycentric coordinates.
 bei_pseudopoints <- rbind(bei_int_matrix, bei_bary)
-
-# Get the numerical integration weights for the SPDE approach.
-# Because the whole region was fully observed (constant sampling effort),
-# these are left as is. If parts of the region were unobserved, these would be
-# multiplied by the observed proportion of the area represented by the node.
-bei_int_weights <- diag(inla.mesh.fem(bei_mesh)$c0)
-
-# Concatenate the weight vector with a vector of zeros for the observed events.
-# This is the vector of Poisson exposure parameters for the pseudodata.
-bei_pseudodata_exp <- c(bei_int_weights, rep(0, bei_n_events))
-
-# Create the psuedodata. This is a vector giving the count of events at each
-# pseudopoint location, that is 0 at each mesh node and 1 at each event.
-bei_pseudodata <- c(rep(0, bei_mesh_size), rep(1, bei_n_events))
 
 
 ########################################
@@ -200,9 +196,10 @@ plot(im(t(inla.mesh.project(bei_proj, bei_result$summary.random$idx$mean)),
         xrange = Frame(bei)$x,
         yrange = Frame(bei)$y,
         unitname = c('meter', 'meters')),
-        main = 'Posterior Mean of Latent GP')
+     riblab = expression(E(bold(e)(u)*'|'*bold(x))),
+     main = 'Posterior Predicted Mean of Latent GP')
 plot(Window(bei), border = 'white', add = TRUE)
-points(bei, pch = '.', col = 'white')
+points(bei, pch = '.', col = '#ffffff80')
 dev.off()
 
 # Plot the posterior standard deviation of the latent surface.
@@ -212,10 +209,33 @@ plot(im(t(inla.mesh.project(bei_proj, bei_result$summary.random$idx$sd)),
         xrange = Frame(bei)$x,
         yrange = Frame(bei)$y,
         unitname = c('meter', 'meters')),
-        main = 'Posterior SD of Latent GP')
+     main = 'Posterior Prediction SD of Latent GP')
 plot(Window(bei), border = 'white', add = TRUE)
-points(bei, pch = '.', col = 'white')
+points(bei, pch = '.', col = '#ffffff80')
 dev.off()
+
+# Plot the posterior mean of the linear predictor.
+pdf('figures/beibetas.pdf', width = 12, height = 6)
+par(mar = c(0.5, 0, 2, 2))
+plot(im(t(
+          bei_result$summary.fixed['intercept', 'mean'] +
+          bei_result$summary.fixed['elev', 'mean'] *
+            inla.mesh.project(bei_proj, bei_mesh_elev) +
+          bei_result$summary.fixed['grad', 'mean'] *
+            inla.mesh.project(bei_proj, bei_mesh_grad)
+        )),
+        xrange = Frame(bei)$x,
+        yrange = Frame(bei)$y,
+        unitname = c('meter', 'meters'),
+     riblab = expression(E(beta[0]*'|'*bold(x)) +
+                         E(beta[1]*'|'*bold(x)) * z[1](u) +
+                         E(beta[2]*'|'*bold(x)) * z[2](u)),
+     main = 'Posterior Mean of Fixed Effects')
+plot(Window(bei), border = 'white', add = TRUE)
+# TODO: figure out why the dots are in the wrong places.
+points(bei, pch = '.', col = '#ffffff80')
+dev.off()
+
 
 # Plot the backtransformed posterior mean of the intensity surface.
 pdf('figures/beiintensity.pdf', width = 12, height = 6)
@@ -231,29 +251,34 @@ plot(im(t(exp(
         xrange = Frame(bei)$x,
         yrange = Frame(bei)$y,
         unitname = c('meter', 'meters')),
-        main = 'Posterior Intensity Function')
+     main = 'Posterior Intensity Function')
 plot(Window(bei), border = 'white', add = TRUE)
-points(bei, pch = '.', col = 'white')
+points(bei, pch = '.', col = '#80808080')
 dev.off()
 
 
 # Plot posterior marginals of the covariance parameters and intercept.
 pdf('figures/beipost.pdf', width = 18, height = 6)
-par(mfrow = c(1, 3))
+par(mfrow = c(1, 3), bty = 'n')
 plot(inla.smarginal(bei_result$marginals.hyperpar$Theta1), type = 'l',
-     main = 'Posterior Distribution of Theta1')
+     yaxt = 'n', xlab = expression(tau),
+     main = expression(paste(bold('Posterior Distribution of '), tau)))
 plot(inla.smarginal(bei_result$marginals.hyperpar$Theta2), type = 'l',
-     main = 'Posterior Distribution of Theta2')
+     yaxt = 'n', xlab = expression(kappa),
+     main = expression(paste(bold('Posterior Distribution of '), kappa)))
 plot(inla.smarginal(bei_result$marginals.fixed$intercept), type = 'l',
-     main = 'Posterior Distribution of Intercerpt')
+     yaxt = 'n', xlab = expression(beta[0]),
+     main = 'Posterior Distribution of the Intercept')
 dev.off()
 
 # Plot posterior marginals of the coefficients.
 pdf('figures/beipostcoefs.pdf', width = 12, height = 6)
-par(mfrow = c(1, 2))
+par(mfrow = c(1, 2), bty = 'n')
 plot(inla.smarginal(bei_result$marginals.fixed$elev), type = 'l',
+     yaxt = 'n', xlab = expression(beta[1]),
      main = 'Posterior Distribution of Elevation Coefficient')
 plot(inla.smarginal(bei_result$marginals.fixed$grad), type = 'l',
+     yaxt = 'n', xlab = expression(beta[2]),
      main = 'Posterior Distribution of Gradient Coefficient')
 dev.off()
 
@@ -312,6 +337,7 @@ bei_resid_df$expected <- bei_resid_df$intensity * bei_resid_df$area
 bei_resid_df$pearson <- (bei_resid_df$count - bei_resid_df$expected) /
   sqrt(bei_resid_df$expected)
 
+# Gridded Pearson residual plot.
 pdf('figures/beiresiduals.pdf', width = 12, height = 6)
 par(mar = c(0.5, 0, 2, 2))
 plot(im(t(matrix(bei_resid_df$pearson, nrow = length(unique(bei_resid_df$x)))),
@@ -319,5 +345,39 @@ plot(im(t(matrix(bei_resid_df$pearson, nrow = length(unique(bei_resid_df$x)))),
         unitname = c('meter', 'meters')),
      main = 'Gridded Pearson Residuals')
 plot(Window(bei), border = 'white', add = TRUE)
-points(bei, pch = '.', col = 'white')
+points(bei, pch = '.', col = '#ffffff80')
+dev.off()
+
+# Set up a projection from the SPDE representation to the event locations.
+bei_event_proj <- inla.mesh.projector(bei_mesh,
+  loc = as.matrix(as.data.frame(bei)))
+
+# Create a copy of bei and mark with 1/sqrt(lambda).
+bei_marked <- bei
+marks(bei_marked) <- as.vector(1/sqrt(exp(
+  bei_result$summary.fixed['intercept', 'mean'] +
+    bei_result$summary.fixed['elev', 'mean'] *
+  inla.mesh.project(bei_event_proj, bei_mesh_elev) +
+    bei_result$summary.fixed['grad', 'mean'] *
+  inla.mesh.project(bei_event_proj, bei_mesh_grad) +
+  inla.mesh.project(bei_event_proj, bei_result$summary.random$idx$mean)
+)))
+
+# Mark plot.
+pdf('figures/beimarkplot.pdf', width = 12, height = 6)
+par(mar = c(0.5, 0, 2, 2))
+plot(im(t(sqrt(exp(
+          bei_result$summary.fixed['intercept', 'mean'] +
+          bei_result$summary.fixed['elev', 'mean'] *
+            inla.mesh.project(bei_proj, bei_mesh_elev) +
+          bei_result$summary.fixed['grad', 'mean'] *
+            inla.mesh.project(bei_proj, bei_mesh_grad) +
+          inla.mesh.project(bei_proj, bei_result$summary.random$idx$mean)
+        ))),
+        xrange = Frame(bei)$x,
+        yrange = Frame(bei)$y,
+        unitname = c('meter', 'meters')),
+        main = 'Mark Plot')
+plot(Window(bei), border = 'white', add = TRUE)
+plot(bei_marked, col = '#ffffff80', add = TRUE)
 dev.off()
